4671fcb6cc4c5383b47e7958418e9df103b1d9f9
Bryan Curillo <bryan.curillo.est@tecazuay.edu.ec>
2022-04-05 18:16:32 -0500

    todos los datos cargados y validados en el modificar enfermera

--- Asilo_anciano_1.0/src/ventanas/crud_enfermera.java
+++ Asilo_anciano_1.0/src/ventanas/crud_enfermera.java
@@ -312,6 +312,8 @@ public class crud_enfermera extends javax.swing.JFrame {
         }
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
