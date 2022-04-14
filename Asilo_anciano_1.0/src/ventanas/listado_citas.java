88b363f2def40ea3ec483a8912110a1e9e35c60a
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2022-04-13 19:32:51 -0500

    Listado_citas

--- Asilo_anciano_1.0/src/ventanas/listado_citas.java
+++ Asilo_anciano_1.0/src/ventanas/listado_citas.java
@@ -1,17 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ventanas;
 
-/**
- *
- * @author Usuario
- */
+import clases.doctor;
+import clases.paciente;
+import clases.cita_medica;
+import conexion_bada.Conexion;
+import conexion_bada.Insert;
+import conexion_bada.Insert_ChequeoMedico;
+import conexion_bada.Insert_doctor;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 public class listado_citas extends javax.swing.JFrame {
 
-    
+    Insert_ChequeoMedico inserchequeo = new Insert_ChequeoMedico();
+
     public listado_citas() {
         initComponents();
         this.setLocationRelativeTo(null);
@@ -28,11 +35,11 @@ public class listado_citas extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        txtCedula = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         boton_regresar_menu_doctor = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        TablaChequeosDoctor = new javax.swing.JTable();
         jButton3 = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
@@ -43,12 +50,17 @@ public class listado_citas extends javax.swing.JFrame {
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("LISTADO DE PROXIMOS CHEQUEOS MEDICOS");
+        jLabel1.setText("LISTADO DE CHEQUEOS MEDICOS");
 
         jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/buscar (2).png"))); // NOI18N
         jButton1.setToolTipText("BUSCAR PACIENTE");
         jButton1.setBorder(null);
         jButton1.setOpaque(false);
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         boton_regresar_menu_doctor.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/atras.png"))); // NOI18N
         boton_regresar_menu_doctor.setToolTipText("regresar");
@@ -60,18 +72,18 @@ public class listado_citas extends javax.swing.JFrame {
             }
         });
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        TablaChequeosDoctor.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null}
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null}
             },
             new String [] {
-                "Cedula del Paciente", " Nombres del Paciente", "Apellidos del Paciente", "Nombre del Doctor", "Apellido del Doctor", "Especialidad del Doctor", "Fecha Chequeo ", "Hora  Chequeo"
+                "Cedula del Paciente", " Nombres del Paciente", "Apellidos del Paciente", "Fecha Chequeo ", "Hora  Chequeo"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(TablaChequeosDoctor);
 
         jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/ACTUALIZARTAB.jpeg"))); // NOI18N
         jButton3.setToolTipText("CARGAR DATOS");
@@ -79,7 +91,7 @@ public class listado_citas extends javax.swing.JFrame {
         jButton3.setOpaque(false);
 
         jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jLabel2.setText("DIGITE CEDULA A BUSCAR:");
+        jLabel2.setText("DIGITE SU NÚMERO DE CÉDULA:");
 
         jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/hogar_de_los_ancianos__1_-removebg-preview (2).png"))); // NOI18N
 
@@ -92,26 +104,20 @@ public class listado_citas extends javax.swing.JFrame {
                 .addComponent(jScrollPane1)
                 .addContainerGap())
             .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(28, 28, 28)
+                .addComponent(jLabel2)
+                .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addContainerGap(227, Short.MAX_VALUE)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                                .addComponent(boton_regresar_menu_doctor)
-                                .addGap(162, 162, 162))
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel1)
-                                .addGap(170, 170, 170))))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(28, 28, 28)
-                        .addComponent(jLabel2)
-                        .addGap(18, 18, 18)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 222, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(txtCedula, javax.swing.GroupLayout.PREFERRED_SIZE, 222, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(30, 30, 30)
                         .addComponent(jButton1)
                         .addGap(58, 58, 58)
                         .addComponent(jButton3)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                        .addGap(47, 47, 47)
+                        .addComponent(boton_regresar_menu_doctor))
+                    .addComponent(jLabel1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 135, Short.MAX_VALUE)
                 .addComponent(jLabel3))
         );
         jPanel1Layout.setVerticalGroup(
@@ -123,7 +129,7 @@ public class listado_citas extends javax.swing.JFrame {
                         .addComponent(jLabel1)
                         .addGap(42, 42, 42)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtCedula, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel2)
                             .addComponent(jButton1)
                             .addComponent(jButton3)
@@ -156,6 +162,63 @@ public class listado_citas extends javax.swing.JFrame {
         acdoctor.setVisible(true);
         dispose();
     }//GEN-LAST:event_boton_regresar_menu_doctorActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        FiltrarDatosChequeoTabla();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    public void FiltrarDatosChequeoTabla() {
+
+        String cedulaAux = txtCedula.getText();
+        int codDoc = 0;
+
+        Insert_ChequeoMedico inserchek = new Insert_ChequeoMedico();
+
+        List<doctor> com = inserchek.ListaChequeoDoctor();
+
+        for (int i = 0; i < com.size(); i++) {
+            if (com.get(i).getCedula().equalsIgnoreCase(cedulaAux)) {
+
+                codDoc = com.get(i).getCodigo();
+            }
+        }
+
+        System.out.println("Bien codDoc: " + codDoc);
+
+        var pacidosfiltro = new ArrayList<paciente>();
+        inserchequeo.ListaChequeoPacienteDos(codDoc).forEach((e) -> {
+            pacidosfiltro.add(e);
+        });
+
+        String matriz[][] = new String[pacidosfiltro.size()][6];
+
+        for (int i = 0; i < pacidosfiltro.size(); i++) {
+
+            matriz[i][0] = pacidosfiltro.get(i).getCedula();
+            matriz[i][1] = pacidosfiltro.get(i).getPri_nomb() + " " + pacidosfiltro.get(i).getSeg_nombre();
+            matriz[i][2] = pacidosfiltro.get(i).getPrim_apell() + " " + pacidosfiltro.get(i).getSeg_apelli();
+
+        }
+
+        var citfiltro = new ArrayList<clases.cita_medica>();
+        inserchequeo.ListaChequeoDos(codDoc).forEach((e) -> {
+            citfiltro.add(e);
+        });
+
+        for (int j = 0; j < citfiltro.size(); j++) {
+
+            matriz[j][3] = citfiltro.get(j).getFecha_chequeoActual();
+            matriz[j][4] = citfiltro.get(j).getHoraChequeo();
+
+        }
+        
+        TablaChequeosDoctor.setModel(new javax.swing.table.DefaultTableModel(
+                matriz,
+                new String[]{
+                    "Cédula del paciente", "Nombres del paciente", "Apellidos del paciente", "Fecha del chequeo médico", "Hora del chequeo médico"
+                }
+        ));
+    }
 
     /**
      * @param args the command line arguments
@@ -193,6 +256,7 @@ public class listado_citas extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTable TablaChequeosDoctor;
     private javax.swing.JButton boton_regresar_menu_doctor;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton3;
@@ -201,7 +265,6 @@ public class listado_citas extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField txtCedula;
     // End of variables declaration//GEN-END:variables
 }
