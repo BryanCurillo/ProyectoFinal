08b742f24cee4e4f627a4c8790640193ed39dd60
Bryan Curillo <bryan.curillo.est@tecazuay.edu.ec>
2022-04-17 15:23:42 -0500

    comento para subir

--- Asilo_anciano_1.0/src/conexion_bada/Insert_tratamiento.java
+++ Asilo_anciano_1.0/src/conexion_bada/Insert_tratamiento.java
@@ -16,12 +16,12 @@ public class Insert_tratamiento extends Tratamiento{
     
     Conexion cone = new Conexion();
     
-    public boolean Insertar_tratamiento() {
-        String sql = "INSERT INTO tratamiento(\n"
-                + "	trar_codigo, trat_descripcion ,trat_fecha_inicio, trat_fecha_fin,trat_codigo_paciente   , his_observaciones)\n"
-                + "	VALUES ('" + getCodigo_tratamiento()+ "', '" + getDescripcion_tratamiento()+ "', '" + getFecha_inicio()+ "', '" + getFecha_fin()+ "', '" + getCodigo_paciente()+ "', '" + getTra_observaciones()+  "');";
-        return cone.InsertUpdateDeleteAcciones(sql);
-    }
+//    public boolean Insertar_tratamiento() {
+//        String sql = "INSERT INTO tratamiento(\n"
+//                + "	trar_codigo, trat_descripcion ,trat_fecha_inicio, trat_fecha_fin,trat_codigo_paciente   , his_observaciones)\n"
+//                + "	VALUES ('" + getCodigo_tratamiento()+ "', '" + getDescripcion_tratamiento()+ "', '" + getFecha_inicio()+ "', '" + getFecha_fin()+ "', '" + getCodigo_paciente()+ "', '" + getTra_observaciones()+  "');";
+//        return cone.InsertUpdateDeleteAcciones(sql);
+//    }
     
     public  List<Tratamiento> lista_tratamiento(){
        String sqls = "select * from tratamiento join paciente on tratamiento.trat_codigo = paciente.paci_codigo;";
@@ -36,7 +36,7 @@ public class Insert_tratamiento extends Tratamiento{
                 mi_trata.setFecha_inicio(trata.getDate("trat_fecha_inicio"));
                 mi_trata.setFecha_fin(trata.getDate("trat_fecha_fin"));
                 mi_trata.setCodigo_paciente(trata.getString("trat_codigo_paciente"));
-                mi_trata.setTra_observaciones(trata.getString("his_observaciones"));
+//                mi_trata.setTra_observaciones(trata.getString("his_observaciones"));
                 
                 list_tratamiento.add(mi_trata);
                 
