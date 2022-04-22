3675786ddb510d72d23be29f60cdb30900ba789f
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2022-04-22 05:00:23 -0500

    CRUD administrador

--- Asilo_anciano_1.0/src/ventanas/crud_administrador.java
+++ Asilo_anciano_1.0/src/ventanas/crud_administrador.java
@@ -152,9 +152,14 @@ public class crud_administrador extends javax.swing.JFrame {
     private void BtRegresarAdministradorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtRegresarAdministradorActionPerformed
 //        this.dispose();
 //        new inicio_frame().setVisible(true);
-          login_administrador mi_loginAdmin = new login_administrador();
-          mi_loginAdmin.setVisible(true);
-          dispose();
+
+//          login_administrador mi_loginAdmin = new login_administrador();
+//          mi_loginAdmin.setVisible(true);
+//          dispose();
+
+            acceso_administrador mi_accesoAdmin = new acceso_administrador();
+            mi_accesoAdmin.setVisible(true);
+            dispose();
     }//GEN-LAST:event_BtRegresarAdministradorActionPerformed
 
     private void BtEliminarAdministradorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtEliminarAdministradorActionPerformed
