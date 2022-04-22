f7366827c3d9f8a8e6de780fa2741ad1b069f09e
Bryan Curillo <bryan.curillo.est@tecazuay.edu.ec>
2022-04-21 23:57:09 -0500

    errores arreglados

--- Asilo_anciano_1.0/src/conexion_bada/Insert_historial.java
+++ Asilo_anciano_1.0/src/conexion_bada/Insert_historial.java
@@ -7,6 +7,8 @@ package conexion_bada;
 
 import clases.Tratamiento;
 import clases.historial_medico;
+import clases.paciente;
+import clases.persona;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -20,8 +22,8 @@ public class Insert_historial extends historial_medico {
 
     public boolean Insertar_historial() {
         String sql = "INSERT INTO historial(\n"
-                + "	his_codigo, his_codigo_paciente, his_codigo_tratamiento, his_codigo_ficha, his_observaciones)\n"
-                + "	VALUES ('" + getCodigo_historial() + "','" + getCodigo_paciente() + "','" + getCodigo_tratamiento() + "','" + getCodigo_ficha() + "','" + getObservaciones_historial() + "');";
+                + "	his_codigo_paciente, his_codigo_tratamiento, his_codigo_ficha)\n"
+                + "	VALUES ('" + getCodigo_paciente() + "','" + getCodigo_tratamiento() + "','" + getCodigo_ficha() + "');";
         return cone.InsertUpdateDeleteAcciones(sql);
     }
 
@@ -66,7 +68,7 @@ public class Insert_historial extends historial_medico {
 
     public int obtenerCodFicha(int codpaci) {
         int codigo = 0;
-        String sqls = "select ficha_codigo from ficha where ficha_paci_codigo=" + codpaci+ ";";
+        String sqls = "select ficha_codigo from ficha where ficha_paci_codigo=" + codpaci + ";";
         ResultSet ru = cone.selectConsulta(sqls);
         try {
             while (ru.next()) {
@@ -76,5 +78,99 @@ public class Insert_historial extends historial_medico {
             Logger.getLogger(Insert.class.getName()).log(Level.SEVERE, null, ex);
         }
         return codigo;
+    }
+
+    public String obtenerObservaciones(int codigo) throws SQLException {
+
+        String observacion = "";
+        String sqls = "select ficha_observaciones from ficha where ficha_paci_codigo=" + codigo + ";";
+        ResultSet obs = cone.selectConsulta(sqls);
+//        try {catch
+        while (obs.next()) {
+            observacion = obs.getString("ficha_observaciones");
+        }
+        return observacion;
+    }
+
+    public List<paciente> ListaPaciente() {
+        String sqls = "select * from persona join paciente on persona.per_cedula = paciente.paci_cedula;";
+        ResultSet respaci = cone.selectConsulta(sqls);
+        List<paciente> paci = new ArrayList<>();
+
+        try {
+            while (respaci.next()) {
+
+                paciente mi_paciente = new paciente();
+                persona mi_persona = new persona();
+
+//                        System.out.println("coninciden");
+                mi_paciente.setCodigo(respaci.getInt("paci_codigo"));
+                mi_paciente.setCedula(respaci.getString("paci_cedula"));
+                mi_paciente.setSeguro(respaci.getString("paci_seguro"));
+                mi_paciente.setFecha_de_ingreso(respaci.getString("paci_fecha_de_ingreso"));
+
+                mi_paciente.setCedula(respaci.getString("per_cedula"));
+                mi_paciente.setPri_nomb(respaci.getString("per_primer_nombre"));
+                mi_paciente.setSeg_nombre(respaci.getString("per_segundo_nombre"));
+                mi_paciente.setPrim_apell(respaci.getString("per_primer_apellido"));
+                mi_paciente.setSeg_apelli(respaci.getString("per_segundo_apellido"));
+                mi_paciente.setCorreo(respaci.getString("per_correo"));
+                mi_paciente.setGenero(respaci.getString("per_genero"));
+                mi_paciente.setDireccion(respaci.getString("per_direccion"));
+                mi_paciente.setTelefono(respaci.getString("per_telefono"));
+                mi_paciente.setTipo_sangre(respaci.getString("per_tipo_sangre"));
+                mi_paciente.setFecha_Nacimiento(respaci.getString("per_fecha_nacimiento"));
+
+                paci.add(mi_paciente);
+//                        resper.close();
+
+            }
+//            respaci.close();
+            return paci;
+        } catch (SQLException ex) {
+            Logger.getLogger(Insert.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+    public List<paciente> ListaPacienteFicha() {
+        String sqls = "select * from persona per ,paciente paci ,ficha where per.per_cedula = paci.paci_cedula and paci.paci_codigo=ficha.ficha_paci_codigo;";
+        ResultSet respaci = cone.selectConsulta(sqls);
+        List<paciente> paci = new ArrayList<>();
+
+        try {
+            while (respaci.next()) {
+
+                paciente mi_paciente = new paciente();
+                persona mi_persona = new persona();
+
+//                        System.out.println("coninciden");
+                mi_paciente.setCodigo(respaci.getInt("paci_codigo"));
+                mi_paciente.setCedula(respaci.getString("paci_cedula"));
+                mi_paciente.setSeguro(respaci.getString("paci_seguro"));
+                mi_paciente.setFecha_de_ingreso(respaci.getString("paci_fecha_de_ingreso"));
+
+                mi_paciente.setCedula(respaci.getString("per_cedula"));
+                mi_paciente.setPri_nomb(respaci.getString("per_primer_nombre"));
+                mi_paciente.setSeg_nombre(respaci.getString("per_segundo_nombre"));
+                mi_paciente.setPrim_apell(respaci.getString("per_primer_apellido"));
+                mi_paciente.setSeg_apelli(respaci.getString("per_segundo_apellido"));
+                mi_paciente.setCorreo(respaci.getString("per_correo"));
+                mi_paciente.setGenero(respaci.getString("per_genero"));
+                mi_paciente.setDireccion(respaci.getString("per_direccion"));
+                mi_paciente.setTelefono(respaci.getString("per_telefono"));
+                mi_paciente.setTipo_sangre(respaci.getString("per_tipo_sangre"));
+                mi_paciente.setFecha_Nacimiento(respaci.getString("per_fecha_nacimiento"));
+
+                paci.add(mi_paciente);
+//                        resper.close();
+
+            }
+//            respaci.close();
+            return paci;
+        } catch (SQLException ex) {
+            Logger.getLogger(Insert.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
     }
 }
