476935da877819c1e20ff772479ba37cb6835c3e
Bryan Curillo <bryan.curillo.est@tecazuay.edu.ec>
2022-04-21 23:54:34 -0500

    registrar admin desde login

--- Asilo_anciano_1.0/src/ventanas/login_administrador.java
+++ Asilo_anciano_1.0/src/ventanas/login_administrador.java
@@ -26,8 +26,9 @@ public class login_administrador extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         txtUsuarioA = new javax.swing.JTextField();
         txtContraseniaA = new javax.swing.JPasswordField();
-        BotonIniciarSesionA = new javax.swing.JButton();
+        BotonRegistrarAdmin = new javax.swing.JButton();
         boton_regresar_menu_principal = new javax.swing.JButton();
+        BotonIniciarSesionA1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -54,15 +55,15 @@ public class login_administrador extends javax.swing.JFrame {
         txtContraseniaA.setToolTipText("INGRESE SU CONTRASEÑA");
         getContentPane().add(txtContraseniaA, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 320, 140, 30));
 
-        BotonIniciarSesionA.setFont(new java.awt.Font("Monospaced", 1, 14)); // NOI18N
-        BotonIniciarSesionA.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/iniciar-sesion (1).png"))); // NOI18N
-        BotonIniciarSesionA.setText("INICIAR SESION");
-        BotonIniciarSesionA.addActionListener(new java.awt.event.ActionListener() {
+        BotonRegistrarAdmin.setFont(new java.awt.Font("Monospaced", 1, 14)); // NOI18N
+        BotonRegistrarAdmin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/registro.png"))); // NOI18N
+        BotonRegistrarAdmin.setText("REGISTRAR");
+        BotonRegistrarAdmin.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                BotonIniciarSesionAActionPerformed(evt);
+                BotonRegistrarAdminActionPerformed(evt);
             }
         });
-        getContentPane().add(BotonIniciarSesionA, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 390, -1, -1));
+        getContentPane().add(BotonRegistrarAdmin, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 410, 140, 30));
 
         boton_regresar_menu_principal.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/atras.png"))); // NOI18N
         boton_regresar_menu_principal.setToolTipText("REGRESAR MENU PRINCIPAL");
@@ -75,15 +76,25 @@ public class login_administrador extends javax.swing.JFrame {
         });
         getContentPane().add(boton_regresar_menu_principal, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 10, -1, -1));
 
+        BotonIniciarSesionA1.setFont(new java.awt.Font("Monospaced", 1, 14)); // NOI18N
+        BotonIniciarSesionA1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/iniciar-sesion (1).png"))); // NOI18N
+        BotonIniciarSesionA1.setText("INICIAR SESION");
+        BotonIniciarSesionA1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BotonIniciarSesionA1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(BotonIniciarSesionA1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 370, -1, 30));
+
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/teahub.io-full-hd-vertical-wallpapers-874835.png"))); // NOI18N
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void BotonIniciarSesionAActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonIniciarSesionAActionPerformed
-        LoginAdministrador();
-    }//GEN-LAST:event_BotonIniciarSesionAActionPerformed
+    private void BotonRegistrarAdminActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonRegistrarAdminActionPerformed
+        new Agregar_administrador(true).setVisible(true);
+    }//GEN-LAST:event_BotonRegistrarAdminActionPerformed
 
     private void boton_regresar_menu_principalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_boton_regresar_menu_principalActionPerformed
         // TODO add your handling code here:
@@ -91,6 +102,10 @@ public class login_administrador extends javax.swing.JFrame {
         menup.setVisible(true);
         dispose();
     }//GEN-LAST:event_boton_regresar_menu_principalActionPerformed
+
+    private void BotonIniciarSesionA1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonIniciarSesionA1ActionPerformed
+        LoginAdministrador();
+    }//GEN-LAST:event_BotonIniciarSesionA1ActionPerformed
 
     public void LoginAdministrador() {
         Insert_usuario miInsert = new Insert_usuario();
@@ -154,7 +169,8 @@ public class login_administrador extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton BotonIniciarSesionA;
+    private javax.swing.JButton BotonIniciarSesionA1;
+    private javax.swing.JButton BotonRegistrarAdmin;
     private javax.swing.JButton boton_regresar_menu_principal;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
